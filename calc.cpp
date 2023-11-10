d7aa5cadd0371c92dec9079bd91a158c767d5587
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-11-10 16:23:31 +0300

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
-using std::cout, std::cin, std::endl;
+using std::cout, std::string, std::cin, std::endl;
 int main() {
 double num1, num2, num3;
-char x;
+string x;
 while (true) {
 cin >> num1 >> x >> num2;
 if (x == "+") {num3 = num1 + num2;}
 if (x == "-") {num3 = num1 - num2;}
-if (x == "/" or x == ":") {num3 = num1 / num2;}      
-if (x == "x" or x == "*") {num3 = num1 * num2;}
-cout << num3 << endl;}}
+if (x == "/" or x == ":") {num3 = num1 / num2;}                                  if (x == "x" or x == "*") {num3 = num1 * num2;}
+cout << num3 << endl;}
+return 0;
+}
