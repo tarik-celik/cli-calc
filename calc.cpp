a37a1cd40ed4018746d94d9fb6babf37e1f27fa0
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-11-10 16:15:11 +0300

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -6,7 +6,7 @@ double num1, num2, num3;
 string x;
 while (true) {
 cin >> num1 >> x >> num2;
-if (x == "+") {num3 = num1 + num2;}
-if (x == "-") {num3 = num1 - num2;}
-if (x == "/" or x == ":") {num3 = num1 / num2;}      if (x == "x" or x == "*") {num3 = num1 * num2;}
+if (x == "+") {num1 + num2 = num3;}
+if (x == "-") {num1 - num2 = num3;}
+if (x == "/" or x == ":") {num1 / num2 = num3;}      if (x == "x" or x == "*") {num1 * num2 = num3;}
 cout << num3 << endl;}}
