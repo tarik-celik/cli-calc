a3c4692df7c21f845c60535a3879ddbfb5d4d1f0
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-11-10 15:52:31 +0300

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using std::cout, std::cin, std::string;
+int main() {
+double num1, num2, num3;
+string x;
+while (true) {
+cin >> num1 >> x >> num2;
+if (x == "+") {num3 = num1 + num2;}
+if (x == "-") {num3 = num1 - num2;}
+if (x == "/" or x == ":") {num3 = num1 / num2;}
+if (x == "x" or x == "*") {num3 = num1 * num2;}                         }
+  cout << num3 << endl}}
