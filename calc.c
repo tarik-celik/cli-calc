ea669673ef15983e9f8f8a2ae56487eebf5b727c
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-12-22 18:11:16 +0300

    Add files via upload

--- calc.c
+++ calc.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main() {
-float num1, num2, num3;
-char op;                                                      
-while(true) {                                                    
-    scanf("%f %c %f", &num1, &op, &num2);
-if (op == *"+") {
-num3 = num1 + num2;
-printf("%.3f",num3);
+
+float *num1 = (float *) malloc (sizeof(float));
+float *num2 = (float *) malloc (sizeof(float));
+float *num3 = (float *) malloc (sizeof(float));
+char *op = (char *) malloc (1);
+
+scanf("%f %c %f", num1, op, num2);
+
+if (*op == *"+") {
+*num3 = *num1 + *num2;
 }
-if (op == *"-") {
-num3 = num1 - num2;
-printf("%.3f", num3);
+
+if (*op == *"-") {
+*num3 = *num1 - *num2;
 }
-if (op == *"/" || op == *":") {
-num3 = num1 / num2;
-printf("%.3f", num3);
+
+if (*op == *"/" || *op == *":") {
+*num3 = *num1 / *num2;
 }
-if (op == *"x" || op == *"*") {
-num3 = num1 * num2;
-printf("%.3f", num3);
+
+if (*op == *"x" || *op == *"*") {
+*num3 = *num1 * *num2;
 }
+
+printf("%.3f", *num3);
 printf("\n");
-}
+
+free(num1);
+free(num2);
+free(num3);
+free(op);
 return 0;
 }
